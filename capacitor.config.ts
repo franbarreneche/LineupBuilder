9f489c4ed76f03acee9f0db6f4218b06358d521d
franbarreneche <franbarreneche@gmail.com>
2022-01-12 22:17:50 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PruebaLineupBuilder',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
